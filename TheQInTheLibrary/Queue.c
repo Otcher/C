a24b4c03863a3a390dc4ce99dab504291a1924d0
Omri <omimako@gmail.com>
2017-02-22 16:57:59 +0200

    Changed Queue completely
    Queue is now constructed out of a list.

--- TheQInTheLibrary/Queue.c
+++ TheQInTheLibrary/Queue.c
@@ -1,84 +1,67 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "Queue.h"
+#include "List.h"
 
+/*
+	Initalize the queue by creating a new list with one node containing the info
+*/
 queue_a* que_init(void* info)
 {
 	queue_a* queue = malloc(sizeof(queue_a));
 	if (queue != NULL)
 	{
-		node_a* node = malloc(sizeof(node_a));
-		if (node != NULL)
-		{
-			node->info = info;
-			node->before = NULL;
+		queue->ls = lst_init(info);
 
-			queue->top_node = node;
-			queue->last_node = node;
-			queue->cell_count = 1;
-
-			return queue;
-		}
+		return queue;
 	}
 }
 
+/*
+	Destroy the queue by deleting all nodes from the list
+*/
 void que_destroy(queue_a * q)
 {
-	/*
-	
-		TODO!
-	
-	*/
+	lst_destroy(q->ls);
 }
 
+/*
+	Insert a new value into the Queue
+*/
 node_a * que_insert(queue_a * q, void * info)
 {
-	node_a* node = malloc(sizeof(node_a));
-	if (node == NULL) return;
-
-	node_a* oldlast = q->last_node;
-	node->info = info;
-	node->before = NULL;
-	oldlast->before = node;
-	q->last_node = node;
+	list_a *lst = q->ls;
+	node_a *node = lst_addnode(lst, info);
 
 	return node;
 }
 
+/*
+	Pop the first node in queue
+*/
 void * que_pop(queue_a * q)
 {
-	if (q->top_node == NULL) return;
-	node_a* topop = q->top_node;
-	q->top_node = topop->before;
+	list_a *lst = q->ls;
+	node_a *node = lst_getfirst(lst);
 
-	void* info = topop->info;
+	void *info = lst_getinfo(node);
+	lst_delete(lst, node);
 
-	free(topop);
-	topop = NULL;
-	--q->cell_count;
 	return info;
 }
 
+/*
+	Return length of queue
+*/
 int que_length(queue_a * q)
 {
-	return q->cell_count;
+	return (lst_getlength(q->ls));
 }
 
-int que_top(queue_a * q)
+/*
+	Return the value of the top of the queue without removing the node
+*/
+void* que_top(queue_a * q)
 {
-	return ((node_a*)q->top_node)->info;
-}
-
-void* que_getinfo(node_a* node)
-{
-	return node->info;
-}
-
-void que_print(queue_a* q)
-{
-	while (q->top_node != NULL)
-	{
-		void* i = que_pop(q);
-		printf("%p,", i);
-	}
+	return (lst_getinfo(lst_getfirst(q->ls)));
 }
--- TheQInTheLibrary/Queue.h
+++ TheQInTheLibrary/Queue.h
@@ -2,32 +2,19 @@
 #define DYN_QUEUE_H
 
 /*
-	Node type struct
-*/
-typedef struct node {
-	void *info;
-	struct node *before;
-} node_a;
-
-/*
 	Queue type struct
 */
 typedef struct queue {
-	struct node_a *top_node;
-	struct node_a *last_node;
-	int cell_count;
+	struct list_a* ls;
 } queue_a;
 
-//funcs
+//Functions
 
 queue_a* que_init(void* info);
 void que_destroy(queue_a* q);
 node_a* que_insert(queue_a* q, void *info);
 void* que_pop(queue_a* q);
 int que_length(queue_a* q);
-int que_top(queue_a* q);
-
-void* que_getinfo(node_a);
-void que_print(queue_a* q);
+void* que_top(queue_a* q);
 
 #endif
