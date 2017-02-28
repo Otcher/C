a02a32359142c8f5a1e7e8522e5fd60661d16983
Omri <omimako@gmail.com>
2017-02-28 13:48:16 +0200

    Added GoL (Game of Life)
    Added FINISHED GoL game

---
+++ GoL/main.c
@@ -0,0 +1,111 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#ifdef __unix__
+#define CLEAR_COMMAND "clear"
+#elif defined(_WIN32) || defined(WIN32) 
+#define CLEAR_COMMAND "cls"
+#endif
+
+#define BOARD_SIZE 50
+#define GAME_SPEED_IN_MS 200
+
+typedef enum {DEAD = 0, ALIVE} cell_status;
+typedef enum { FALSE = 0, TRUE } boolean;
+
+void PrintCells(int board[][BOARD_SIZE])
+{
+	for (int y = 0; y < BOARD_SIZE;y++)
+	{
+		for (int x= 0; x < BOARD_SIZE; x++)
+		{
+			if (board[x][y] == ALIVE) printf("%c", 'X');
+			else printf("%c", '.');
+		}
+		printf("%c", '\n');
+	}
+}
+
+int GetCellSum(int board[][BOARD_SIZE], int x, int y)
+{
+	int sum = 0;
+	for (int i = (x-1); i <= (x+1); i++)
+	{
+		for (int j = (y-1); j <= (y+1); j++)
+		{
+			if (i == x && j == y) continue;
+			if (x >= 0 && y >= 0 && x < BOARD_SIZE && y < BOARD_SIZE)
+			{
+				sum += board[i][j];
+			}
+		}
+	}
+
+	return sum;
+}
+
+int main()
+{
+	int board[BOARD_SIZE][BOARD_SIZE];
+	int newBoard[BOARD_SIZE][BOARD_SIZE];
+	memset(board, DEAD, sizeof(board));
+	memset(newBoard, DEAD, sizeof(newBoard));
+
+	/*
+		Initializing the board with example cells
+	*/
+
+	board[20][20] = ALIVE;
+	board[21][20] = ALIVE;
+	board[22][20] = ALIVE;
+	board[24][20] = ALIVE;
+	board[20][21] = ALIVE;
+	board[23][22] = ALIVE;
+	board[24][22] = ALIVE;
+	board[21][23] = ALIVE;
+	board[22][23] = ALIVE;
+	board[24][23] = ALIVE;
+	board[20][24] = ALIVE;
+	board[22][24] = ALIVE;
+	board[24][24] = ALIVE;
+
+	/*------------------------------
+		Game Loop
+	------------------------------*/
+	PrintCells(board);
+
+	while (TRUE)
+	{
+		memcpy(newBoard, board, sizeof(board));
+		for (int y = 0; y < BOARD_SIZE; y++)
+		{
+			for (int x = 0; x < BOARD_SIZE; x++)
+			{
+				/*Check if the cell is located in a problematic location (e.g [0, something] or [something, BOARD_SIZE]*/
+				int sum = GetCellSum(board, x, y);
+				if (board[x][y] == ALIVE)
+				{
+					/*Check if lonely or overcrowded then kill, if not then keep the cell alive*/
+					if (sum < 2 || sum > 3)
+					{
+						newBoard[x][y] = DEAD;
+					}
+				}
+				else
+				{
+					/*Check if its a boy or a girl (kidding, check if there is a baby to deliver or not)*/
+					if(sum == 3)
+						newBoard[x][y] = ALIVE;
+				}
+			}
+		}
+
+		/*Copy the new board to the old one*/
+		memcpy(board, newBoard, sizeof(newBoard));
+
+		system(CLEAR_COMMAND);
+		PrintCells(board);
+		_sleep(GAME_SPEED_IN_MS);
+	}
+	return 0;
+}
